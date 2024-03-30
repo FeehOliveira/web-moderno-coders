e5be4fbaf06abc32a5cea72319850a4cf978553a
Felipe Oliveira <feeholiveira10@outlook.com>
2024-03-30 06:38:53 -0300

    sistema de modulos

---
+++ node/moduloCliente.js
@@ -0,0 +1,13 @@
+const moduloA = require('./moduloA');
+
+const moduloB = require('./moduloB');
+
+console.log(moduloA);
+console.log(moduloA.bemVindo);
+console.log(moduloB);
+
+//função 
+
+console.log(moduloB.boaNoite());
+
+
